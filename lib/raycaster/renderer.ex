40ccab6c60f3f302ea0234211b51a5b7cec716e7
Josh Adams <josh.rubyist@gmail.com>
2016-05-06 13:00:16 -0500

    Get a mouse-tracking circle going

--- lib/raycaster/renderer.ex
+++ lib/raycaster/renderer.ex
@@ -1,22 +1,28 @@
 defmodule Raycaster.Renderer do
   @behaviour :wx_object
-  alias Extris.Shapes
+  @timer_interval 20
 
   require Record
   Record.defrecordp :wx, Record.extract(:wx, from_lib: "wx/include/wx.hrl")
   Record.defrecordp :wxSize, Record.extract(:wxSize, from_lib: "wx/include/wx.hrl")
   Record.defrecordp :wxKey, Record.extract(:wxKey, from_lib: "wx/include/wx.hrl")
+  Record.defrecordp :wxPaint, Record.extract(:wxPaint, from_lib: "wx/include/wx.hrl")
+  Record.defrecordp :wxCommand, Record.extract(:wxCommand, from_lib: "wx/include/wx.hrl")
+  Record.defrecordp :wxMouse, Record.extract(:wxMouse, from_lib: "wx/include/wx.hrl")
 
   defmodule State do
     defstruct [
       :parent,
+      :config,
       :canvas,
+      :bitmap,
+      :overlay,
+      :pos,
       :timer
     ]
   end
 
   def start_link(config) do
-    IO.puts "starting"
     :wx_object.start_link(__MODULE__, config, [])
   end
 
@@ -25,63 +31,142 @@ defmodule Raycaster.Renderer do
   end
 
   def do_init(config) do
-    parent = :proplists.get_value(:parent, config)
-    canvas = :wxBufferedPaintDC.new(parent)
-    # :wxWindow.hide(parent)
-    # :wxWindow.reparent(canvas, parent)
-    # :wxWindow.show(parent)
+    parent = config[:parent]
+    panel = :wxPanel.new(parent, [])
 
-    state = %State{
-      parent: parent,
-      canvas: canvas
-    }
+    ## Setup sizers
+    main_sizer = :wxBoxSizer.new(:wx_const.wx_vertical)
+    sizer = :wxStaticBoxSizer.new(:wx_const.wx_vertical, panel)
 
-    timer = :timer.send_interval(20, self, :update)
+    canvas = :wxPanel.new(panel, style: :wx_const.wx_full_repaint_on_resize)
 
-    {parent, %State{ state | timer: timer } }
+    :wxPanel.connect(canvas, :paint, [:callback])
+    :wxPanel.connect(canvas, :size)
+    :wxPanel.connect(canvas, :left_down)
+    :wxPanel.connect(canvas, :left_up)
+    :wxPanel.connect(canvas, :motion)
+
+    ## Add to sizers
+    :wxSizer.add(sizer, canvas, flag: :wx_const.wx_expand, proportion: 1)
+
+    :wxSizer.add(main_sizer, sizer, flag: :wx_const.wx_expand, proportion: 1)
+
+    :wxPanel.setSizer(panel, main_sizer)
+    :wxSizer.layout(main_sizer)
+
+    {w,h} = :wxPanel.getSize(canvas)
+    bitmap = :wxBitmap.new(:erlang.max(w,30),:erlang.max(30,h))
+
+    timer = :timer.send_interval(@timer_interval, self, :tick)
+
+    {panel, %State{parent: panel, config: config, canvas: canvas, bitmap: bitmap, pos: {0, 0}, overlay: :wxOverlay.new(), timer: timer}}
   end
 
-  def handle_info(:update, state) do
-    IO.puts "update"
-    new_state = :wx.batch(fn() -> render(state) end)
-    {:noreply, new_state}
+  ########
+  ## Sync event from callback events, paint event must be handled in callbacks
+  ## otherwise nothing will be drawn on windows.
+  def handle_sync_event(wx(event: wxPaint()), _wxObj, %State{canvas: canvas, bitmap: bitmap}) do
+    dc = :wxPaintDC.new(canvas)
+    redraw(dc, bitmap)
+    :wxPaintDC.destroy(dc)
+    :ok
   end
 
-  def handle_info(:stop, state) do
-    :timer.cancel(state.timer)
-    {:stop, :normal, state}
+  def draw_stuff(state) do
+    fun = fn(dc) ->
+      :wxDC.clear(dc)
+      :wxDC.setBrush(dc, :wx_const.wx_red_brush)
+      :wxDC.drawCircle(dc, state.pos, 15)
+    end
+    draw(state.canvas, state.bitmap, fun)
   end
 
-  def handle_call(msg, _from, state) do
-    {:reply, :ok, state}
+  def handle_event(wx(event: wxSize(size: {w, h})), state = %State{bitmap: prev, canvas: canvas}) do
+    bitmap = :wxBitmap.new(w,h)
+    draw(canvas, bitmap, fn(dc) -> :wxDC.clear(dc) end)
+    :wxBitmap.destroy(prev)
+    {:noreply, %State{state | bitmap: bitmap}}
   end
 
-  def handle_cast(msg, state) do
+  def handle_event(wx(event: wxMouse(type: :left_down, x: x, y: y)), state) do
+    {:noreply, %State{state|pos: {x, y}}}
+  end
+
+  def handle_event(wx(event: wxMouse(type: :motion, x: x, y: y)), state = %State{overlay: overlay, canvas: canvas}) do
+    {:noreply, %State{state | pos: {x, y}}}
+  end
+
+  def handle_event(ev = wx(), state = %State{}) do
     {:noreply, state}
   end
 
-  def handle_event(msg, state) do
+  ## Callbacks handled as normal gen_server callbacks
+  def handle_info(:tick, state) do
+    draw_stuff(state)
+    {:noreply, state}
+  end
+  def handle_info(msg, state) do
+    {:noreply, state}
+  end
+
+  def handle_call(:shutdown, _from, state = %State{parent: panel, timer: timer}) do
+    :wxPanel.destroy(panel)
+    :timer.cancel(timer)
+    {:stop, :normal, :ok, state}
+  end
+  def handle_call(msg, _from, state) do
+    {:reply,{:error, :nyi}, state}
+  end
+
+  def handle_cast(msg, state) do
+    IO.puts "Got cast #{inspect msg}"
     {:noreply, state}
   end
 
   def code_change(_, _, state) do
-    {:stop, :not_yet_implemented, state}
+    {:stop, :ignore, state}
   end
 
-  def terminate(_reason, state) do
-    :timer.cancel(state.timer)
-    :timer.sleep(300)
+  def terminate(_reason, %State{overlay: overlay, timer: timer}) do
+    :timer.cancel(timer)
+    :wxOverlay.destroy(overlay)
+    :ok
   end
 
-  def render(state) do
-    draw(state)
+  #####
+  ## Local functions
+  #####
+
+  ## Buffered makes it all appear on the screen at the same time
+  def draw(canvas, bitmap, fun) do
+    memory_dc = :wxMemoryDC.new(bitmap)
+    fun.(memory_dc)
+
+    cdc = :wxWindowDC.new(canvas)
+    :wxDC.blit(
+      cdc,
+      {0,0},
+      {:wxBitmap.getWidth(bitmap), :wxBitmap.getHeight(bitmap)},
+      memory_dc,
+      {0,0}
+    )
+    :wxWindowDC.destroy(cdc)
+    :wxMemoryDC.destroy(memory_dc)
   end
 
-  def draw(state) do
-    :wxPaintDC.clear(state.canvas)
+  def redraw(dc, bitmap) do
+    memory_dc = :wxMemoryDC.new(bitmap)
+    :wxDC.blit(
+      dc,
+      {0,0},
+      {:wxBitmap.getWidth(bitmap), :wxBitmap.getHeight(bitmap)},
+      memory_dc,
+      {0,0}
+    )
+    :wxMemoryDC.destroy(memory_dc)
+  end
 
-    # TODO: Draw a polygon somewhere
-
-    state
+  def get_pos(w,h) do
+    {:random.uniform(w), :random.uniform(h)}
   end
 end
--- src/wx_const.erl
+++ src/wx_const.erl
@@ -24,9 +24,29 @@ wx_full_repaint_on_resize() ->
 wx_italic_font() ->
     ?wxITALIC_FONT.
 
-
 wx_id_any() ->
     ?wxID_ANY.
 
 wx_sunken_border() ->
     ?wxSUNKEN_BORDER.
+
+wx_transparent_brush() ->
+    ?wxTRANSPARENT_BRUSH.
+
+wx_transparent_pen() ->
+    ?wxTRANSPARENT_PEN.
+
+wx_black() ->
+    ?wxBLACK.
+
+wx_red_brush() ->
+    ?wxRED_BRUSH.
+
+wx_blue_brush() ->
+    ?wxBLUE_BRUSH.
+
+wx_green_brush() ->
+    ?wxGREEN_BRUSH.
+
+wx_light_grey_pen() ->
+    ?wxLIGHT_GREY_PEN.
